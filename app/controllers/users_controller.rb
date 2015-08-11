cbc878a917ae979a22d4ec9d3bf8e0de496516d3
Ryan Dorr <ryandorr17@gmail.com>
2015-08-10 20:13:48 -0500

    Add sign in logic and routing to videos

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,14 +3,19 @@ class UsersController < ApplicationController
   def sign_in
 
     # i'm unable to access the api documenation at this time, I'm assuming the login
-    # method accepts a hash and returns nil if not valid
+    # method accepts a hash and
+    #####
+    ##  Big Assumption: returns nil if not valid
+    ####
     user = User.login(user_params)
 
     if user
-     # Log the user in and redirect to the user's show page.
+      session[:user_id] = user.id
+      current_user
+      redirect_to videos_path
     else
-     # Create an error message.
-     render 'new'
+     flash.now[:danger] = 'Invalid email/password combination'
+     render 'sign_in'
     end
 
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+
+  root 'videos#index'
+
+  get 'videos'   =>   'videos#index'
+  get 'sign_in'  =>   'users#sign_in'
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
