da7535bd8c8f38a09acb691ad687758d2a0dee7c
Ryan Dorr <ryandorr17@gmail.com>
2015-08-10 19:36:36 -0500

    Add get methods to User and Video models.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRestClient::Base
   base_url Rails.application.config.api_server_url
 
+  get :find, '/users/:id'
+  get :login, '/login?email=:email&password=:password'
+
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,6 @@
 class Video < ActiveRestClient::Base
   base_url Rails.application.config.api_server_url
 
+  get :featured, '/videos/featured'
+
 end
