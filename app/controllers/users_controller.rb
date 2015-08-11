355d1077a2740af9a9c2277bd70f95bc4d268e8e
Ryan Dorr <ryandorr17@gmail.com>
2015-08-10 20:01:39 -0500

    Finish the user sign in page.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,17 @@
 class UsersController < ApplicationController
 
   def sign_in
-    User.login(user_params)
+
+    # i'm unable to access the api documenation at this time, I'm assuming the login
+    # method accepts a hash and returns nil if not valid
+    user = User.login(user_params)
+
+    if user
+     # Log the user in and redirect to the user's show page.
+    else
+     # Create an error message.
+     render 'new'
+    end
 
   end
 
